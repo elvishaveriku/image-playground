f99f64fa7948253d689d16d399e2133f724d4653
elvishaveriku <144339266+elvishaveriku@users.noreply.github.com>
2023-10-10 17:27:24 +0200

    Add files via upload

---
+++ images.py
@@ -0,0 +1,30 @@
+from PIL import Image, ImageFilter
+
+# img = Image.open("./astro.jpg")
+# img.thumbnail((400, 200))
+# img.save('thumbnail.jpg')
+# filtered_img = img.filter(ImageFilter.BLUR)
+# filtered_img.save("blur.png", 'png')
+# filtered_img = img.filter(ImageFilter.SMOOTH)
+# filtered_img.save("smooth.png", 'png')
+# filtered_img = img.filter(ImageFilter.SHARPEN)
+# filtered_img.save("sharp.png", 'png')
+# filtered_img = img.convert('L')
+# filtered_img.save("grey.png", 'png')
+#
+# crooked = filtered_img.rotate(180)
+# crooked.save("grey.png", 'png')
+#
+# resize = filtered_img.resize((300, 300))
+# resize.save("grey.png", 'png')
+#
+# box = (100, 100, 400, 400)
+# region = filtered_img.crop(box)
+# region.save("grey.png", 'png')
+
+# print(img)
+# #print(img.format)
+# print(img.size)
+# print(img.mode)
+
+# jpg might give an error, so use png
